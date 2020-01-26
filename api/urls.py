c0e802252b73e6ea1cab6d68e4b071f7dd45fd82
Rémi Rosenthal <remi-rosenthal@hotmail.fr>
2020-01-26 19:46:30 +0100

    custom auth token response

--- api/urls.py
+++ api/urls.py
@@ -1,12 +1,9 @@
 from django.urls import path
 
-from rest_framework.authtoken.views import obtain_auth_token
-
-from .views import ProductList, ProductDetail
-
+from .views import CustomObtainAuthToken, ProductList, ProductDetail
 
 urlpatterns = [
     path('product/', ProductList.as_view()),
     path('product/<int:pk>', ProductDetail.as_view()),
-    path('api-auth-token/', obtain_auth_token, name='api_auth_token'),
+    path('api-auth-token/', CustomObtainAuthToken.as_view(), name='api_auth_token'),
 ]
--- api/views.py
+++ api/views.py
@@ -1,8 +1,19 @@
 from rest_framework import generics
+from rest_framework.authtoken.models import Token
+from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
 from .models import Product
 from .serializers import ProductSerializer
+
+
+class CustomObtainAuthToken(ObtainAuthToken):
+
+    def post(self, request, *args, **kwargs):
+        response = super(CustomObtainAuthToken, self).post(request, *args, **kwargs)
+        token = Token.objects.get(key=response.data['token'])
+        return Response({'token': token.key, 'id': token.user_id})
 
 
 class ProductList(generics.ListCreateAPIView):
